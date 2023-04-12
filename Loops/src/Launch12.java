20afe7517705a131bea2e60ff44419acc71dfb42
tanishq-1012 <tanishq1012@yahoo.com>
2023-04-12 22:19:31 +0530

    New Loop program

---
+++ Loops/src/Launch12.java
@@ -0,0 +1,12 @@
+
+public class Launch12 {
+
+	public static void main(String[] args) {
+		int[] a = {10,20,30,40,50};
+		
+		for(int i=0;i<a.length;i++) {
+			System.out.println("Marks of student "+(i+1)+" : "+a[i]);
+		}
+	}
+
+}
