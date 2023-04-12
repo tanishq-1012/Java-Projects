6ea3d9576ccf7d52a58e3a8094f5bf2dd3ee8fec
tanishq-1012 <tanishq1012@yahoo.com>
2023-04-12 20:34:37 +0530

    First Commit

---
+++ DemoApp/src/DemoApp.java
@@ -0,0 +1,16 @@
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		int[] a = {10,20,30,40};
+		
+		for(int ele : a) {
+			System.out.println(ele);
+		}
+		
+		String s = "Tanishq";
+		int count = s.toUpperCase().length();
+		System.out.println(count);
+	}
+
+}
